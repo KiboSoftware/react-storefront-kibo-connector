c6a2c395118495ec63b9a8bed911b1a4c4cf83c2
SushantJadhav <Sushant.Jadhav@kibocommerce.com>
2021-08-16 15:57:13 +0530

    Initial Commit

---
+++ src/getContent/index.ts
@@ -0,0 +1,21 @@
+
+import get from 'lodash/get'
+import getClient from '../util/client'
+import defaultQuery from './query';
+
+export default async function getContent(params, req, res) {
+    const variables = { documentListName: params.documentType, filter: `properties.tags eq ${params.slug}` }
+
+    const client = getClient(req, res)
+
+    const response = await client.query({
+        query: defaultQuery, variables: variables, context: {
+            headers: {
+                'x-vol-user-claims': null
+            }
+        }
+    })
+
+    console.log(`get Content: `, response)
+    return response;
+}
